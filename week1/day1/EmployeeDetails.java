e8735ddc724a6cddd0777d88f466c79f3e57d843
Arunsekar Panneer Selvam <68766275+Arunsekar2681997@users.noreply.github.com>
2025-10-12 09:52:19 +0530

    Add files via upload
    Week1.Day1 Home Assignments

---
+++ week1/day1/EmployeeDetails.java
@@ -0,0 +1,31 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		
+		String name = "John Doe";
+		int age = 30;
+		double salary = 55000.75;
+		char gender='M';
+		boolean acceptTerms = true;
+		float rating = 4.5F;
+		long mobileNumber = 9876543210L;
+		int pinCode = 56001;
+		int luckyNumber = 7;
+		
+		System.out.println("Below The Employee Details");
+		System.out.println("--------------------------");
+		System.out.println("Printing The Name :"+name);
+		System.out.println("Printing The Age :"+age);
+		System.out.println("Printing The Salary :"+salary);
+		System.out.println("Printing The Gender :"+gender);
+		System.out.println("Accepting The Terms :"+acceptTerms);
+		System.out.println("Printing The Rating :"+rating);
+		System.out.println("Printing The Mobile Number :"+mobileNumber);
+		System.out.println("Printing The PinCode :"+pinCode);
+		System.out.println("Printing The Lucky Number :"+luckyNumber);
+
+	}
+
+}
